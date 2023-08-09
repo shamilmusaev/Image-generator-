e4dc2da1740b20c982e4c5d27b6f6e6b4f6fa840
Dwhitewolf1995 <shamil.musaev@iths.se>
2023-08-09 17:45:32 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+async function query(data) {
+	const response = await fetch(
+		"https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5",
+		{
+			headers: { Authorization: "Bearer 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" },
+			method: "POST",
+			body: JSON.stringify(data),
+		}
+	);
+	const result = await response.blob();
+	return result;
+}
+
+const generateButton = document.getElementById("send-button");
+const messageInput = document.getElementById("message-input");
+const messagesContainer = document.querySelector(".messages");
+
+generateButton.addEventListener("click", async () => {
+  const messageText = messageInput.value;
+  if (messageText.trim() !== "") {
+    const messageElement = document.createElement("div");
+    messageElement.classList.add("message", "user-message");
+    messageElement.textContent = messageText;
+    messagesContainer.appendChild(messageElement);
+    messageInput.value = "";
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+
+    const inputText = messageText; // Используем введенный текст для генерации
+    const imageData = await query({ inputs: inputText });
+    const imageUrl = URL.createObjectURL(imageData);
+
+    const imageElement = document.createElement("img");
+    imageElement.classList.add("img_container");
+    imageElement.src = imageUrl;
+    messagesContainer.appendChild(imageElement);
+    
+    
+  }
+});
