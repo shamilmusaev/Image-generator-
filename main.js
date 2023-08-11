f147cf30764faf3c868efb63c82264d0f60b4bbc
Dwhitewolf1995 <shamil.musaev@iths.se>
2023-08-11 21:59:47 +0200

    more configs

--- main.js
+++ main.js
@@ -1,14 +1,27 @@
 async function query(data) {
-	const response = await fetch(
-		"https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5",
-		{
-			headers: { Authorization: "Bearer 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" },
-			method: "POST",
-			body: JSON.stringify(data),
-		}
-	);
-	const result = await response.blob();
-	return result;
+  const response = await fetch(
+    "https://api-inference.huggingface.co/models/runwayml/stable-diffusion-v1-5",
+    {
+      headers: { Authorization: "Bearer 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" },
+      method: "POST",
+      body: JSON.stringify(data),
+    }
+  );
+  const result = await response.blob();
+  return result;
+}
+
+async function animateMessage(messageText) {
+  const messageElement = document.createElement("div");
+  messageElement.classList.add("message", "user-message");
+  messagesContainer.appendChild(messageElement);
+
+  const delay = 70;
+
+  for (let i = 0; i <= messageText.length; i++) {
+    messageElement.textContent = messageText.substring(0, i);
+    await new Promise(resolve => setTimeout(resolve, delay));
+  }
 }
 
 const generateButton = document.getElementById("send-button");
@@ -18,23 +31,69 @@ const messagesContainer = document.querySelector(".messages");
 generateButton.addEventListener("click", async () => {
   const messageText = messageInput.value;
   if (messageText.trim() !== "") {
-    const messageElement = document.createElement("div");
-    messageElement.classList.add("message", "user-message");
-    messageElement.textContent = messageText;
-    messagesContainer.appendChild(messageElement);
+    const userMessageElement = document.createElement("div");
+    userMessageElement.classList.add("message", "user-message");
+    userMessageElement.textContent = messageText;
+    messagesContainer.appendChild(userMessageElement);
     messageInput.value = "";
     messagesContainer.scrollTop = messagesContainer.scrollHeight;
 
-    const inputText = messageText; // Используем введенный текст для генерации
-    const imageData = await query({ inputs: inputText });
+    const loadingIndicator = document.createElement("div");
+    loadingIndicator.classList.add("message", "other-message", "typing-indicator");
+    const dots = document.createElement("span");
+    dots.classList.add("dot");
+    loadingIndicator.appendChild(dots);
+    loadingIndicator.appendChild(dots.cloneNode());
+    loadingIndicator.appendChild(dots.cloneNode());
+    messagesContainer.appendChild(loadingIndicator);
+
+    const imageData = await query({ inputs: messageText });
     const imageUrl = URL.createObjectURL(imageData);
+
+    messagesContainer.removeChild(loadingIndicator);
 
     const imageElement = document.createElement("img");
     imageElement.classList.add("img_container");
     imageElement.src = imageUrl;
-    imageElement.alt = "Not availble";
+    imageElement.alt = "Not available";
     messagesContainer.appendChild(imageElement);
-    
+
+
+    // Прокрутить контейнер к последнему элементу
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
     
   }
 });
+
+
+document.addEventListener("DOMContentLoaded", async function () {
+  const messagesContainer = document.querySelector(".messages");
+  const delay = 10;
+
+  const messages = [
+    "Hello. Type a prompt and get awesome images 😜",
+    "For example ",
+    "Space scene in Van Gogh style",
+    "Cityscape at night in cyberpunk style"
+  ];
+
+  async function animateMessage(message) {
+    const messageElement = document.createElement("div");
+    messageElement.classList.add("other-message");
+    messagesContainer.appendChild(messageElement);
+
+    for (let i = 0; i <= message.length; i++) {
+      messageElement.textContent = message.substring(0, i);
+      await new Promise(resolve => setTimeout(resolve, delay));
+    }
+
+    await new Promise(resolve => setTimeout(resolve, 1000)); // Задержка перед следующим сообщением
+  }
+
+  for (const message of messages) {
+    await animateMessage(message);
+  }
+});
+
+
+
